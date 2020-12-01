52062fe61f17349f9181f7c0c92a03e46d1cc781
Dudemanjude <dudemanjude@gmail.com>
2020-11-30 22:08:10 -0800

    Add hugo site

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var numberBox = document.querySelector('#numberBox');
 var calculateButton = document.querySelector('#calculateFactors');
 var factorZone = document.querySelector('#factorZone');
+var currentNumber = document.querySelector('#currentNumber');
 
 var factors = [];
 var squareFactors = [];
@@ -10,19 +11,24 @@ function calculateFactors() {
     clearFactorZone();
     factors = [];
     squareFactors = [];
-    
-    for (let i = 0; i <= num; i++) {
+    var timeBeforeCalculatingFactors = Date.now();
+    for (let i = 0; i <= num/2; i++) {
         if (num % i == 0) {
             factors.push(i);
         }
     }
+    factors.push(num);
+    console.log("Calculating factors took: " + (Date.now() - timeBeforeCalculatingFactors) + " ms");
     console.log(factors);
+    var timeBeforeCalculatingSquareFactors = Date.now();
     for (let factor = 0; factor < factors.length; factor++) {
         const element = factors[factor];
         if (Number.isInteger(Math.sqrt(element))) {
             squareFactors.push(element);
         }
     }
+    console.log("Calculating square factors took: " + (Date.now() - timeBeforeCalculatingSquareFactors) + " ms");
+    console.log(squareFactors);
     var factorsElement = document.createElement('p')
     factorsElement.innerText = "Here are the factors: " + factors
     factorZone.append(factorsElement);
@@ -31,7 +37,6 @@ function calculateFactors() {
     squareFactorsElement.innerText = "Here are the square factors: " + squareFactors
     factorZone.append(squareFactorsElement);
 
-    console.log(squareFactors);
 }
 
 function clearFactorZone() {
